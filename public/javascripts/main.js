83d3ed75116708383f40bb03309f759d4913ba40
Nguyen Thi Ngoc Tram <ntntram.013@gmail.com>
2021-01-12 15:59:56 +0700

    Change date locale to VN

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -754,3 +754,29 @@ $(document).ready(function() {
 
 
 });
+/*---------------------
+    23. Paypal-payment
+  --------------------- */
+/*
+paypal.Buttons({
+    style:{
+        color: 'blue',
+        shape: 'pill'
+    },
+    createOrder: function (data, actions) {
+        return actions.order.create({
+            purchase_unit:[{
+                amount:{
+                    value:'0.1'
+                }
+            }]
+        })
+    },
+    onApprove: function (data, actions) {
+        return actions.order.capture().then(function (details) {
+            console.log(details);
+        })
+    }
+    }
+).render('#paypal-payment-button');
+*/
